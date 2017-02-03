d0e40218ce4bbb7514ffde420525e3c26608c507
giulio ungaretti <giulioungaretti@me.com>
2017-02-03 12:30:08 +0100

    fix -y

--- update.cmd
+++ update.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-ECHO "updating qcodes" > nul
+ECHO "updating qcodes" nul
 CALL activate qcodes-qdev-master > nul
-CALL conda upgrade --all > nul
+CALL conda upgrade --all -y
 CALL pip install -U https://github.com/qdev-dk/Qcodes/archive/master.zip 
 pause
