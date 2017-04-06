ab7ceae4932fe8dec0e09038324f01c93a802d2a
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2017-04-06 10:37:35 +0200

    Only pip upgrade dependencies if needed
    prevent pip from unconditionally upgrading dependencies. 
    I.e conda currently installes setuptools 27.x and a dependency of 
    qcodes depends on setuptools so pip install qcodes upgrades setuptools 
    which breaks because it is installed from conda.

--- update.cmd
+++ update.cmd
@@ -2,5 +2,5 @@
 ECHO "updating qcodes" nul
 CALL activate qcodes-qdev-master > nul
 CALL conda upgrade --all -y
-CALL pip install -U https://github.com/qdev-dk/Qcodes/archive/master.zip 
+CALL pip install -U https://github.com/qdev-dk/Qcodes/archive/master.zip --upgrade-strategy only-if-needed
 pause
