540f898928d4c50f074af6d7df2deee3952bec9e
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2017-08-21 16:22:39 +0200

    Install pywin32 and correct broadbean url

--- install.cmd
+++ install.cmd
@@ -9,9 +9,10 @@ CALL %UserProfile%\Anaconda3\Scripts\conda create -y -n qcodes-qdev-master pytho
 CALL %UserProfile%\Anaconda3\Scripts\activate qcodes-qdev-master
 CALL %UserProfile%\Anaconda3\Scripts\conda install -y h5py
 CALL %UserProfile%\Anaconda3\Scripts\conda install -y matplotlib
+CALL %UserProfile%\Anaconda3\Scripts\conda install -y pywin32
 call %userprofile%\Anaconda3\envs\qcodes-qdev-master\scripts\pip install pyqtgraph
 ECHO "Installing final bits.."
 CALL %UserProfile%\Anaconda3\envs\qcodes-qdev-master\Scripts\pip install https://github.com/qdev-dk/Qcodes/archive/master.zip
-CALL %UserProfile%\Anaconda3\envs\qcodes-qdev-master\Scripts\pip install git+https://github.com/QCoDeS/broadbean.git
+CALL %UserProfile%\Anaconda3\envs\qcodes-qdev-master\Scripts\pip install https://github.com/QCoDeS/broadbean/archive/master.zip
 ECHO "DONE, you can close the window now"
 pause
