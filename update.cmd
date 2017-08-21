68bf928306784d93ba02229656c9a26f1dfca46b
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2017-08-21 16:23:11 +0200

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -3,5 +3,5 @@ ECHO "updating qcodes" nul
 CALL activate qcodes-qdev-master > nul
 CALL conda upgrade --all -y
 CALL pip install -U https://github.com/qdev-dk/Qcodes/archive/master.zip --upgrade-strategy only-if-needed
-CALL pip install -U git+https://github.com/QCoDeS/broadbean.git --upgrade-strategy only-if-needed
+CALL pip install -U https://github.com/QCoDeS/broadbean/archive/master.zip --upgrade-strategy only-if-needed
 pause
